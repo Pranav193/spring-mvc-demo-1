8992f89e29a7aac3e50690d47f526e01878b5671
Pranav Parihar <parihar.pranav@yahoo.com>
2019-02-23 21:39:38 +0530

    my first commit

---
+++ src/com/pranav/controllers/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.pranav.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WelcomeController {
+@RequestMapping("/hi")
+	public String doWelcome(Model model) {
+		return "index1";
+	}
+}
